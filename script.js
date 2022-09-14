16fbf8e3cfb306a0f25f3a1a8d48bf30ae55061e
fabralbert <fabrmonk@gmail.com>
2022-09-14 03:23:34 +0300

    Implemented functions to change length and size of numbers

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const deleteBtn = document.querySelector('.calculator_item__delete');
 const clearBtn = document.querySelector('.calculator_item__clear');
 const numbersBtns = document.querySelectorAll('.calculator_item__number');
 const operationsBtns = document.querySelectorAll('.calculator_item__operation');
+const errorText = document.querySelector('.calculator_error')
+
+console.log(errorText.style)
 
 // обработчики
 // кнопки расчета '=', удаления последнего числа '<=', полной очистки (сброс всего) 'C'
@@ -37,13 +40,22 @@ let isLastEqualOperation = false;
 let isLastSelectOperation = false;
 // пометка, что последний раз пользовались вводом числа
 let isLastSelectNumber = false;
+// если был показан текст при делении на 0
+let isLastError = false;
 
 numberInput.textContent = 0;
 
 function selectNumber(event){
+  
   console.log(`%c${'selectNumber:'}`, `color: ${'#FFFF00'}`);
   console.log(`selectNumber Вход, prevNumber: ${previousNumber} number: ${number} operation: ${operation} isLastEqualOperation: ${isLastEqualOperation}`)
 
+  // если была ошибка, все отчищаем
+  if (isLastError) {
+    clear()
+  }
+
+  // если после знака равенства нажали на выбор цифр, все очищаем
   if (isLastEqualOperation){
     clear()
   }
@@ -66,11 +78,26 @@ function selectNumber(event){
     number = number + event.target.textContent;
   }
 
-  updateNodes(operation, number , previousNumber || '')
+  // делим число на две части до точки и после точки в виде массива
+  const lengthOfNumbersAfterDot = number.toString().split('.')
 
+  // если больше 8 цифр после запятой, то приводим к 8 знакам после запятой
+  if (lengthOfNumbersAfterDot[1]?.length > 8){
+    number = parseFloat(number).toFixed(8);
+  }
+
+  // если длина числа до точки больше 16, то обрезать
+  if (lengthOfNumbersAfterDot[0]?.length > 16){
+    number = lengthOfNumbersAfterDot[0].slice(0, -1)
+  }
+
+  updateNodes(operation, parseFloat(number), previousNumber || '')
+  
   isLastSelectNumber = true;
+  // обрабатывает размер текста с учетом количества цифр
+  
 
-
+  changeSizeOfText()
   console.log(`selectNumber Выход, prevNumber: ${previousNumber} number: ${number} operation: ${operation}`)
 }
 
@@ -78,26 +105,33 @@ function selectOperation(event) {
   console.log(`%c${'selectOperation:'}`, `color: ${'#DA70D6'}`);
   console.log(`selectOperation Вход, prevNumber: ${previousNumber} number: ${number} operation: ${operation}`)
 
+  if (isLastError) {
+    clear()
+  }
+
   // если operationInput.textContent равен одно из операторов +,-,/,*, тогда можно его записать в lastOperator
-  if (operationInput.textContent && operationInput.textContent !== '%') {
+  if (operationInput.textContent && operationInput.textContent !== '%' && operationInput.textContent !== '=') {
     lastOperation = operationInput.textContent;
   }
 
   // в случае если lastOperator есть и нажали на %, значит сделать расчет с %.
   if (lastOperation && event.target.textContent === '%'){
+    console.log('lastOperation', lastOperation)
+    console.log('Заход')
     operation = event.target.textContent;
     calculate(event)
     return
   }
 
   if (!operation){
-    previousNumber = number
+    number = parseFloat(number);
+    previousNumber = parseFloat(number)
   }
 
   // если последний раз нажимали кнопку '=', тогда делаем и number и prevNumber = total.
   if (isLastEqualOperation){
-    previousNumber = total || 0;
-    number = total || 0;
+    previousNumber = parseFloat(total) || 0;
+    number = parseFloat(total) || 0;
     total = '';
   }
   console.log(`%c${'selectOperation:'}`, `color: ${'#DA70D6'}`);
@@ -105,10 +139,19 @@ function selectOperation(event) {
 
   // !isLastSelectOperation нужен чтобы вычисление не выполнялось если мы много раз подряд к примеру нажмем '+'
   // !isLastEqualOperation нужен т.к. если мы последний раз нажали '=', и потом нажали еще например + то вычисление уже выполнено, еще одно сразу же не нужно.
-  if (number && !isLastSelectOperation && !isLastEqualOperation && operation){
+  if (!isLastSelectOperation && !isLastEqualOperation && operation && operation !== '%'){
+
+    // проверка деления на ноль
+    if (parseFloat(number) === 0 && operation === '/'){
+      console.log('здесьошибка')
+      showErrorText()
+      return
+    }
+
     calculate(event)
-    previousNumber = total;
-    number = total;
+    console.log('Заходтут')
+    previousNumber = parseFloat(total);
+    number = parseFloat(total);
     total = '';
   }
 
@@ -121,15 +164,24 @@ function selectOperation(event) {
   isLastEqualOperation = false;
   isLastSelectNumber = false;
 
+  changeSizeOfText()
   console.log(`selectOperation Выход, prevNumber: ${previousNumber} number: ${number} operation: ${operation} isLastEqualOperation: ${isLastEqualOperation}`)
 }
 
+// функция расчета
 function calculate(event) {
+  number = parseFloat(number);
+
+  if (isLastError) {
+    clear()
+  }
+
   console.log(`%c${'calculate:'}`, `color: ${'#d77332'}`);
   console.log(`calculate Вход, prevNumber: ${previousNumber} number: ${number} operation: ${operation}`)
 
   if (!operation){
-    previousNumber = number
+    
+    previousNumber = parseFloat(number);
     updateNodes(event.target.textContent, number, previousNumber);
     return
   }
@@ -140,24 +192,30 @@ function calculate(event) {
 
   switch(operation){
     case '/':
+
       if (parseFloat(number) === 0){
-        numberInput.textContent = 'на ноль делить нельзя';
-        return 'деление на ноль'
+        console.log('здесьошибка')
+        showErrorText()
+        return
       }
 
       total = parseFloat(previousNumber) / parseFloat(number);
+      fixNumbersAfterDot()
       break;
 
     case '*':
       total = parseFloat(previousNumber) * parseFloat(number);
+      fixNumbersAfterDot()
       break;
 
     case '+':
       total = parseFloat(previousNumber) + parseFloat(number);
+      fixNumbersAfterDot()
       break;
 
     case '-':
       total = parseFloat(previousNumber) - parseFloat(number);
+      fixNumbersAfterDot()
       break;
     
     case '%':
@@ -167,25 +225,27 @@ function calculate(event) {
 
       if (lastOperation === '*') {
         total = parseFloat(previousNumber) * (parseFloat(number) / 100);
-
+        fixNumbersAfterDot()
       }
 
       // Проверка на то что number не равен нулю
       if (lastOperation === '/' && parseFloat(number)) { 
         console.log(number)
         total = parseFloat(previousNumber) / (parseFloat(number) / 100);
+        fixNumbersAfterDot()
       } else if (lastOperation === '/') {
-        numberInput.textContent = 'на ноль делить нельзя';
-        isLastEqualOperation = true
+        showErrorText()
         return
       }
 
       if (lastOperation === '+') {
         total = parseFloat(previousNumber) + parseFloat(previousNumber) * parseFloat(number) / 100
+        fixNumbersAfterDot()
       }
       
       if (lastOperation === '-') {
         total = parseFloat(previousNumber) - parseFloat(previousNumber) * parseFloat(number) / 100
+        fixNumbersAfterDot()
       }
       break;
 
@@ -203,12 +263,14 @@ function calculate(event) {
   
   // после каждого расчета приравниваем prevNumer к total, чтобы уже на основе нового prevNumber продолжать другие расчеты
   if (!isNaN(total)){
-    previousNumber = total;
+    previousNumber = parseFloat(total);
   }
 
   console.log(`calculate Выход, prevNumber: ${previousNumber} number: ${number} operation: ${operation} total: ${total}`)
 
   isLastSelectNumber = false;
+
+  changeSizeOfText()
 }
 
 function clear() {
@@ -223,15 +285,26 @@ function clear() {
   isLastEqualOperation = false;
   isLastSelectOperation = false;
   isLastSelectNumber = false;
+  isLastError = false;
+  errorText.style.display = 'none'
+  changeSizeOfText();
 }
 
 function deleteLastNumber() {
+  
+  if (isLastError) {
+    clear()
+  }
+
   console.log(`deleteLastNum Вход, prevNumber: ${previousNumber} number: ${number} operation: ${operation} total: ${total}`)
   if (isLastSelectNumber) {
     const editNumber = number.toString().slice(0, -1);
     number = parseFloat(editNumber || 0);
     updateNodes(operation, number, previousNumber || '')
   }
+
+  changeSizeOfText()
+
   console.log(`deleteLastNum Выход, prevNumber: ${previousNumber} number: ${number} operation: ${operation} total: ${total}`)
 }
 
@@ -239,4 +312,57 @@ function updateNodes(operationInp, numberInp, previousNumberInp){
   operationInput.textContent = operationInp;
   numberInput.textContent = numberInp;
   resultInput.textContent = previousNumberInp;
+}
+
+function showErrorText() {
+  updateNodes('', '', '');
+  errorText.style.display = 'block';
+  isLastError = true;
+}
+
+// изменение размера при увеличении количества цифр
+function changeSizeOfText(){
+  if (numberInput.textContent.length < 8) {
+    numberInput.style.fontSize = "100px";
+  };
+  if (numberInput.textContent.length > 8 && numberInput.textContent.length <= 12) {
+    numberInput.style.fontSize = "70px";
+  };
+  if (numberInput.textContent.length > 12 && numberInput.textContent.length <= 14) {
+    numberInput.style.fontSize = "60px";
+  };
+  if (numberInput.textContent.length > 14 && numberInput.textContent.length < 16) {
+    numberInput.style.fontSize = "50px";
+  };
+
+  console.log('length', resultInput.textContent.length)
+  if (resultInput.textContent.length < 8) {
+    operationInput.style.fontSize = "70px";
+    resultInput.style.fontSize = "70px";
+  };
+  if (resultInput.textContent.length > 12 && resultInput.textContent.length <= 22) {
+    operationInput.style.fontSize = "60px";
+    resultInput.style.fontSize = "60px";
+  };
+  if (resultInput.textContent.length > 22 && resultInput.textContent.length <= 32) {
+    operationInput.style.fontSize = "50px";
+    resultInput.style.fontSize = "50px";
+  };
+  if (resultInput.textContent.length > 32 && resultInput.textContent.length <= 40) {
+    operationInput.style.fontSize = "35px";
+    resultInput.style.fontSize = "35px";
+  };
+  if (resultInput.textContent.length > 40) {
+    operationInput.style.fontSize = "22px";
+    resultInput.style.fontSize = "22px";
+  };
+}
+
+function fixNumbersAfterDot(){
+  // смотрим сколько количество цифр после запятой у total
+  const numsAfterDotTotal = total.toString().split('.')[1]?.length;
+
+  if (numsAfterDotTotal >= 1) {
+    total = parseFloat(total.toFixed(8))
+  }
 }
